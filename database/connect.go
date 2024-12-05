7f0985e0db3ab379cf4cb9610157f58085921a3d
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2024-12-05 21:10:55 +0700

    fix: remove ssl_mode

--- database/connect.go
+++ database/connect.go
@@ -21,7 +21,7 @@ func ConnectDB() {
 		log.Println("DB_PORT is missing or invalid in .env file")
 	}
 
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"))
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"))
 	DB, err = gorm.Open(postgres.Open(dsn))
 
 	if err != nil {
